0b43a7449fb37f03e8a752d688a150e80fd6af5f
RoseNFan <48249004+accepterblaketow@users.noreply.github.com>
2021-12-07 16:09:33 +0800

    1

---
+++ bit.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void T1(){
+	
+}
+void T2(){
+	
+}
+void T3(){
+	
+}
+int main(){
+	int T,b;
+	string A,B;
+	cout <<"輸入Type代號(1,2,3):\n";
+	cin >> T;
+	cout <<"輸入基底b:\n";
+	cin >> b;
+	cout <<"輸入A=\n";	
+	cin >> A;
+	cout <<"輸入B=\n";
+	cin >> B;
+	
+	switch(T){
+		case 1:
+			
+	
+	}
+} 
