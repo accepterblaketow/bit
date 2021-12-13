d98e26067214c6e42f92e5f43f53693ed5ca2df0
RoseNFan <48249004+accepterblaketow@users.noreply.github.com>
2021-12-13 21:41:10 +0800

    2

--- bit.cpp
+++ bit.cpp
@@ -1,21 +1,79 @@
 #include<bits/stdc++.h>
 using namespace std;
-void T1(){
+long long T,base,A,B,bit;
+void case1(){
+	vector<char> a(bit,'0'),b(bit,'0');
+	int n=bit-1;
+	while(A){
+		int x=A%base;
+		char c=x+'0';
+		a[n--]=c;
+		A/=base;
+	}
+	n=bit-1;
+	while(B){
+		int x=B%base;
+		char c=x+'0';
+		b[n--]=c;
+		B/=base;
+	}
+	for(int i=0;i<bit;i++){
+		cout <<a[i];
+	}
+	cout <<'\n';
+	for(int i=0;i<bit;i++){
+		cout <<b[i];
+	}
+} 
+void case2(){
 	
+}
+void case3(){
+	
+}
+void case4(){
+	
+}
+void case5(){
+	
+}
+void case6(){
+	
+}
+void case7(){
+	
+}
+void T1(){
+	if(A > 0 && B > 0)
+		case1();
+	else
+		case2();			
 }
 void T2(){
-	
+	if(A > 0 && B > 0)
+		case3();
+	else if(A < 0 && B < 0)
+		case5();
+	else
+		case4();		
 }
 void T3(){
-	
+	if(A > 0 && B > 0)
+		case3();
+	else if(A < 0 && B < 0)
+		case6();
+	else
+		case7();
 }
 int main(){
-	int T,b;
-	string A,B;
+
+	
 	cout <<"輸入Type代號(1,2,3):\n";
 	cin >> T;
 	cout <<"輸入基底b:\n";
-	cin >> b;
+	cin >> base;
+	cout <<"輸入有效位元\n";
+	cin >> bit;
 	cout <<"輸入A=\n";	
 	cin >> A;
 	cout <<"輸入B=\n";
@@ -23,6 +81,8 @@ int main(){
 	
 	switch(T){
 		case 1:
+			T1();
+			break;
 			
 	
 	}
