c744702367c80c791cba1fc86de7eb022cb6d052
RoseNFan <48249004+accepterblaketow@users.noreply.github.com>
2021-12-31 04:20:07 +0800

    l

--- bit.cpp
+++ bit.cpp
@@ -5,36 +5,148 @@ char trans(int x){
 	if(x >= 10)
 		return (x-10)+'A';
 	else
-		return x+'｛｝＼＼＼＼＼＼＼３0';
+		return x+'0';
 	
 }
 void case1(){
 	long long C=A+B; 
-	if(C>pow(2,bit-1)-1)
+	if(C>pow(2,bit-1)-1){
+		cout <<"結果為:";
 		cout <<"溢位\n";
+		return;
+	} 
 	string s;
 	while(C){
 		int x=C%base;
 		char c=trans(x);
-	} 
+		s=c+s;
+		C/=base;
+	}
+	cout <<"結果為:";
+	cout.width(bit);
+	cout.fill('0');
+	cout << s <<'\n';
 } 
 void case2(){
-	
+	long long C=A+B;
+	int sb;
+	if(C>0)
+		sb=0;
+	else
+		sb=1;
+	string s;
+	C=abs(C);
+	while(C){
+		int x=(C%base);
+		char c=trans(x);
+		s=c+s;
+		C/=base;
+	}
+	cout <<"結果為:";
+	cout <<sb;
+	cout.width(bit-1);
+	cout.fill('0');
+	cout  << s <<'\n';
 }
 void case3(){
-	
+	long long C=A+B; 
+	if(C>pow(2,bit-1)-1){
+		cout <<"溢位\n";
+		return;
+	} 
+	string s;
+	while(C){
+		int x=C%base;
+		char c=trans(x);
+		s=c+s;
+		C/=base;
+	}
+	cout <<"結果為:";
+	cout.width(bit);
+	cout.fill('0');
+	cout << s <<'\n';
 }
 void case4(){
-	
+	long long C=A+B;
+	int sb;
+	if(C>0)
+		sb=0;
+	else
+		sb=1;
+	string s;
+	C=abs(C);
+	while(C){
+		int x=(C%base);
+		char c=trans((base-1)-x);
+		s=c+s;
+		C/=base;
+	}
+	cout <<"結果為:";
+	cout <<sb;
+	cout.width(bit-1);
+	cout.fill('0');
+	cout  << s <<'\n';
 }
 void case5(){
-	
+	long long C=A+B; 
+	if(C<-(pow(2,bit-1)-1)){
+		cout <<"溢位\n";
+		return;
+	} 
+	string s;
+	C=abs(C);
+	while(C){
+		int x=C%base;
+		char c=trans((base-1)-x);
+		s=c+s;
+		C/=base;
+	}
+	cout <<"結果為:";
+	cout << 1;
+	cout.width(bit-1);
+	cout.fill('0');
+	cout << s <<'\n';
 }
 void case6(){
-	
+	long long C=A+B; 
+	if(C<-pow(2,bit)){
+		cout <<"溢位\n";
+		return;
+	} 
+	string s;
+	C=pow(2,bit-1)+C;
+	while(C){
+		int x=C%base;
+		char c=trans(x);
+		s=c+s;
+		C/=base;
+	}
+	cout <<"結果為:";
+	cout << 1;
+	cout.width(bit-1);
+	cout.fill('0');
+	cout << s <<'\n';
 }
 void case7(){
-	
+	long long C=A+B;
+	int sb;
+	if(C>0)
+		sb=0;
+	else
+		sb=1;
+	string s;
+	C=pow(2,bit-1)+C;
+	while(C){
+		int x=(C%base);
+		char c=trans(x);
+		s=c+s;
+		C/=base;
+	}
+	cout <<"結果為:";
+	cout <<sb;
+	cout.width(bit-1);
+	cout.fill('0');
+	cout  << s <<'\n';
 }
 void T1(){
 	if(A > 0 && B > 0)
@@ -61,7 +173,7 @@ void T3(){
 int main(){
 
 	
-	cout <<"輸入Type代號(1,2,3):\n";
+	cout <<"輸入Type代號(1:帶號大小,2:基底減一補數,3:基底補數):\n";
 	cin >> T;
 	cout <<"輸入基底b:\n";
 	cin >> base;
@@ -76,7 +188,11 @@ int main(){
 		case 1:
 			T1(); 
 			break;
-			
-	
+		case 2:
+			T2(); 
+			break;
+		case 3:
+			T3(); 
+			break;
 	}
 }  
